e3046c7cd6798418b6b49e098c0433a19ff094b4
Godfirst Wilfred <mayorwizy@gmail.com>
2021-08-25 01:14:30 +0100

    Store and Cart functionality added

--- greatkart/settings.py
+++ greatkart/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     'category',
     'accounts',
     'store',
+    'carts',
 ]
 
 MIDDLEWARE = [
@@ -66,6 +67,7 @@ TEMPLATES = [
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
                 'category.context_processors.menu_links',
+                'carts.context_processors.counter',
             ],
         },
     },
@@ -129,6 +131,6 @@ STATICFILES_DIRS = [
     'greatkart/static',
 ]
 
-# media files configuration 
+# media files configuration
 MEDIA_URL = '/media/'
-MEDIA_ROOT = BASE_DIR /'media'
+MEDIA_ROOT = BASE_DIR / 'media'
